8b4b3e7ad515d90c49ba0929befb02c9f12deb14
djfrancesco <pacullfrancois@gmail.com>
2019-07-16 11:12:02 +0200

    reformated the code

--- scripts/priority_queue_timings.py
+++ scripts/priority_queue_timings.py
@@ -4,13 +4,13 @@ from edsger.priority_queue_timings import sort_compare
 
 res = []
 for i in range(1, 5):
-    n = 10**i
+    n = 10 ** i
     for j in range(3):
         d = sort_compare(n)
-        d['trial'] = j
-        d['n'] = n
+        d["trial"] = j
+        d["n"] = n
         res.append(d)
 res = pd.DataFrame(res)
-res = res.groupby('n').min()
-res.drop('trial', axis=1, inplace=True)
+res = res.groupby("n").min()
+res.drop("trial", axis=1, inplace=True)
 print(res)
