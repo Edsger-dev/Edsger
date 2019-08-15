8be9aff19063999d35f8b1e4b7f96940daf52916
djfrancesco <pacullfrancois@gmail.com>
2019-08-15 10:31:21 +0200

    added some comments

--- src/tests/test_sssp.py
+++ src/tests/test_sssp.py
@@ -19,6 +19,7 @@ def one_edge_01():
 
 @pytest.fixture
 def braess_network_01():
+
     csr_indices = np.array([1, 2, 2, 3, 3], dtype=np.uint32)
     csr_indptr = np.array([0, 2, 4, 5, 5], dtype=np.uint32)
     edge_weights = np.array([1.0, 2.0, 0.0, 2.0, 1.0], dtype=np.float)
@@ -41,62 +42,84 @@ def test_convert_01(one_edge_01):
 
 
 def test_convert_02():
+    """ Test of the conversion from a sorted graph (tail node index first, head node index second) to a CSR format.
+
+    The sorted graph is the following one (Braess-like network):
+    edge index: 0, 1, 2, 3, 4
+    tail nodes: 0, 0, 1, 1, 2
+    head nodes: 1, 2, 2, 3, 3
+    """
+
+    # data creation
     tail_nodes = np.array([0, 0, 1, 1, 2])
     head_nodes = np.array([1, 2, 2, 3, 3])
 
+    #
     indptr_ref = np.array([0, 2, 4, 5, 5], dtype=np.uint32)
     indptr = convert_sorted_graph_to_csr(tail_nodes, head_nodes, 4)
-
     assert isinstance(indptr, np.ndarray)
     np.testing.assert_array_equal(indptr_ref, indptr)
     assert np.issubdtype(indptr.dtype, np.integer)
 
 
 def test_sssp_01(one_edge_01):
+    """ Test of the basic single-source shortest path algorithm on a one-edge network.
+    """
 
+    # data loading
     graph_edges = one_edge_01
+
+    # data preparation
     tail_nodes = graph_edges.tail_vert.values.astype(np.uint32)
     head_nodes = graph_edges.head_vert.values.astype(np.uint32)
     edge_weights = graph_edges.t0.values.astype(np.float64)
-
     csr_indptr = convert_sorted_graph_to_csr(tail_nodes, head_nodes, 2)
     csr_indices = np.copy(head_nodes)
+
+    # origin: node 0
     travel_time_ref = np.array([0.0, 1.0], dtype=np.float)
     travel_time = sssp_basic(csr_indices, csr_indptr, edge_weights, 0, 2)
-
     assert isinstance(travel_time, np.ndarray)
     np.testing.assert_array_equal(travel_time_ref, travel_time)
     assert np.issubdtype(travel_time.dtype, np.floating)
 
+    # origin: node 1
     travel_time_ref = np.array([INFINITY_PY, 0.0], dtype=np.float)
     travel_time = sssp_basic(csr_indices, csr_indptr, edge_weights, 1, 2)
-
     assert isinstance(travel_time, np.ndarray)
     np.testing.assert_array_equal(travel_time_ref, travel_time)
     assert np.issubdtype(travel_time.dtype, np.floating)
 
 
 def test_sssp_02(braess_network_01):
+    """ Test of the basic single-source shortest path algorithm on a Braess-like network.
+    """
+
+    # data loading
     csr_indices, csr_indptr, edge_weights = braess_network_01
 
+    # origin: node 0
     travel_time_ref = np.array([0.0, 1.0, 1.0, 2.0], dtype=np.float)
     travel_time = sssp_basic(csr_indices, csr_indptr, edge_weights, 0, 4)
     assert isinstance(travel_time, np.ndarray)
     np.testing.assert_array_equal(travel_time_ref, travel_time)
     assert np.issubdtype(travel_time.dtype, np.floating)
 
+    # origin: node 1
     travel_time_ref = np.array([INFINITY_PY, 0.0, 0.0, 1.0], dtype=np.float)
     travel_time = sssp_basic(csr_indices, csr_indptr, edge_weights, 1, 4)
     assert isinstance(travel_time, np.ndarray)
     np.testing.assert_array_equal(travel_time_ref, travel_time)
     assert np.issubdtype(travel_time.dtype, np.floating)
 
+    # origin: node 2
     travel_time_ref = np.array([INFINITY_PY, INFINITY_PY, 0.0, 1.0], dtype=np.float)
     travel_time = sssp_basic(csr_indices, csr_indptr, edge_weights, 2, 4)
     assert isinstance(travel_time, np.ndarray)
     np.testing.assert_array_equal(travel_time_ref, travel_time)
     assert np.issubdtype(travel_time.dtype, np.floating)
 
+    # origin: node 3
     travel_time_ref = np.array(
         [INFINITY_PY, INFINITY_PY, INFINITY_PY, 0.0], dtype=np.float
     )
