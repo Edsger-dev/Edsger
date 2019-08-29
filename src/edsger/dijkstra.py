cb10a3ebe6eac1a2164deccf29760c4bbf43ecac
djfrancesco <pacullfrancois@gmail.com>
2019-08-29 08:50:51 +0200

    wip

--- src/edsger/dijkstra.py
+++ src/edsger/dijkstra.py
@@ -4,43 +4,40 @@ from edsger.shortestpath import convert_sorted_graph_to_csr, path_length
 
 
 class Path(object):
-    def __init__(self, edge_df, source="source", target="target", weight="weight"):
+    def __init__(self, edges_df, source="source", target="target", weight="weight"):
         self._edges = None
-        self._load_edges(edge_df, source, target, weight)
+        self._load_edges(edges_df, source, target, weight)
 
-    def _load_edges(self, edge_df, source, target, weight):
+    def _load_edges(self, edges_df, source, target, weight):
 
-        if type(edge_df) != pd.core.frame.DataFrame:
-            raise TypeError("graph_edges should be a pandas DataFrame")
+        if type(edges_df) != pd.core.frame.DataFrame:
+            raise TypeError("edges_df should be a pandas DataFrame")
 
-        if source not in edge_df:
-            raise KeyError(
-                "'source' vertex index column not found in graph edges dataframe"
+        if source not in edges_df:
+            raise KeyError(f"'{source}' column not found in graph edges dataframe")
+
+        if target not in edges_df:
+            raise KeyError(f"'{target}' column not found in graph edges dataframe")
+
+        if weight not in edges_df:
+            raise KeyError(f"'{weight}' column not found in graph edges dataframe")
+
+        if edges_df[[source, target, weight]].isna().any().any():
+            raise ValueError(
+                f"edges_df[[{source}, {target}, {weight}]] should not have missing values"
             )
-
-        if target not in edge_df:
-            raise KeyError(
-                "'target' vertex index column not found in graph edges dataframe"
-            )
-
-        if weight not in edge_df:
-            raise KeyError(
-                "edge attribute 't0' column not found in graph edges dataframe"
-            )
-
-        self._edges = edge_df[[source, target, weight]].copy(deep=True)
 
         for col in [source, target]:
-            if not pd.api.types.is_integer_dtype(self._edges[col].dtype):
-                raise TypeError(
-                    f"The column named '{col}' of the graph edges dataframe should be of integer type"
-                )
+            if not pd.api.types.is_integer_dtype(edges_df[col].dtype):
+                raise TypeError(f"edges_df[{col}] should be of integer type")
 
-        if self._edges[weight].min() < 0.0:
-            raise ValueError(
-                f"edge attribute '{weight}' should be non-negative numbers"
-            )
+        if not pd.api.types.is_numeric_dtype(edges_df[weight].dtype):
+            raise TypeError(f"edges_df[{weight}] should be of numeric type")
 
+        if edges_df[weight].min() < 0.0:
+            raise ValueError(f"edges_df[{weight}] should not be negative")
+
+        self._edges = edges_df[[source, target, weight]].copy(deep=True)
         # if np.amax(self._edges['t0'].values) == np.inf:
         #     raise ValueError("edge attribute 't0' should not be infinite")
 
--- src/tests/test_dijkstra.py
+++ src/tests/test_dijkstra.py
@@ -1,16 +1,43 @@
+import numpy as np
 import pandas as pd
+import pytest
+
 from edsger.dijkstra import Path
 
 
-def test_path_01():
-    """ Braess-like network
-	"""
-    edges = pd.DataFrame(
+@pytest.fixture
+def braess():
+    """ Braess-like graph
+    """
+    edges_df = pd.DataFrame(
         data={
             "source": [0, 0, 1, 1, 2],
             "target": [1, 2, 2, 3, 3],
             "weight": [1.0, 2.0, 0.0, 2.0, 1.0],
         }
     )
-    path = Path(edges)
-    assert True
+    return edges_df
+
+
+def test_load_edges(braess):
+
+    edges_df = braess
+
+    with pytest.raises(TypeError, match=r"should be a pandas DataFrame"):
+        Path("aaa")
+    with pytest.raises(KeyError, match=r"not found in graph edges dataframe"):
+        Path(edges_df, source="tail")
+    with pytest.raises(KeyError, match=r"not found in graph edges dataframe"):
+        Path(edges_df, target="head")
+    with pytest.raises(KeyError, match=r"not found in graph edges dataframe"):
+        Path(edges_df, weight="cost")
+    with pytest.raises(ValueError, match=r"should not have missing values"):
+        Path(edges_df.replace(0, np.nan))
+    with pytest.raises(TypeError, match=r"should be of integer type"):
+        Path(edges_df.astype({"source": float}))
+    with pytest.raises(TypeError, match=r"should be of integer type"):
+        Path(edges_df.astype({"target": float}))
+    with pytest.raises(TypeError, match=r"should be of numeric type"):
+        Path(edges_df.astype({"weight": str}))
+    with pytest.raises(ValueError, match=r"should not be negative"):
+        Path(pd.concat([edges_df[["source", "target"]], edges_df.weight * -1], axis=1))
